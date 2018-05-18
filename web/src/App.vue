deca469f3bc36270a7d93b5cad07aba987783b97
渣臻 <qz519189636@qq.com>
2018-05-18 19:22:25 +0800

    qz

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app-wrapper">
+    <div id="app">
+      <router-view/>
+    </div>
+    <el-footer class="footer">
+      <div class="top">Copyright @ WYUOJ</div>
+      <div class="bottom">Code by ZhuTianQiang</div>
+    </el-footer>
   </div>
 </template>
 
@@ -9,3 +15,31 @@
     name: 'app'
   }
 </script>
+
+<style type="text/css">
+  #app-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  #app {
+    padding-bottom: 60px;
+    width: 100%;
+    height: 100%;
+  }
+
+  .footer {
+    margin-top: -60px;
+    width: 100%;
+    height: 60px;
+    text-align: center;
+    color: white;
+    background-color: #24A3EF;
+  }
+
+  .top {
+    padding-top: 14px;
+  }
+
+</style>
